d00b7250434581ecd6dde708d4b934f932b6705c
bornohin <tonmoyenator@gmail.com>
2023-10-30 15:12:00 -0500

    Update 0350-intersection-of-two-arrays-ii.py

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -13,3 +13,29 @@ class Solution:
                 frequency = min(arr1[item], arr2[item])
                 res.extend([item] * frequency)
         return res
+
+'''# Sorted approach
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1 = sorted(nums1)
+        nums2 = sorted(nums2)
+        i, j = 0, 0
+        res = []
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                res.append(nums1[i])
+                i += 1
+                j += 1
+            else:
+                small_num = nums1[i] if nums1[i] < nums2[j] else nums2[j]
+                if res and nums1[i] == res[-1] and nums2[j] != res[-1]:
+                    i += 1
+                elif res and nums1[i] != res[-1] and nums2[j] == res[-1]:
+                    j += 1
+                else:
+                    if nums1[i] < nums2[j]:
+                        i += 1
+                    else:
+                        j += 1
+        return res
+'''
